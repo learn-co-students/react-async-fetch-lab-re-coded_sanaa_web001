9e6e48f8b2c897c0cd9f706ba8046f8d1b244411
Marwa Alaghbri <marwaalaghbry@gmail.com>
2020-09-21 10:30:18 +0000

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,23 @@
 // create your App component here
+import React from 'react';
+
+class App extends React.Component {
+
+    state = {
+        peopleInSpace: null
+    }
+    render() {
+        return <h1>Hello World! </h1>;
+    }
+
+    componentDidMount() {
+        fetch('http://api.open-notify.org/astros.json')
+            .then(response => response.json())
+            .then(data => {this.setState({
+                    peopleInSpace: data.people
+                })
+              return this.state.peopleInSpace     })
+    }
+  }
+
+export default App
